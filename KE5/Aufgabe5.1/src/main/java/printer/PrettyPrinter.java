a9e64aa6025ea2a83ba7bd2f8c5a713fb7b59eed
dennisfreitag <dennisfreitag@gmail.com>
2019-12-08 12:25:21 +0100

    Aufgabe 5.1 bearbeitet - prettyParser abgeschlossen

--- KE5/Aufgabe5.1/src/main/java/printer/PrettyPrinter.java
+++ KE5/Aufgabe5.1/src/main/java/printer/PrettyPrinter.java
@@ -1,4 +1,11 @@
 package printer;
 
+import interfaces.OutputStream;
+
 public class PrettyPrinter {
+  public void print(OutputStream stream, PrettyPrinterConfiguration configuration) {
+    if (stream != null) {
+      // pretty print the AST to stream;
+    }
+  }
 }
